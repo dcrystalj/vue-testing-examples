3be3e2dbd0ac6631bd563929232bf98a84206dd2
dcrystalj <dcrystalj@gmail.com>
2019-01-07 22:11:39 +0100

    Add 0 class case

--- tests/unit/components/renderedComponentOutput/Classes.spec.js
+++ tests/unit/components/renderedComponentOutput/Classes.spec.js
@@ -3,6 +3,7 @@ import Classes from '@/components/renderedComponentOutput/Classes';
 
 describe('Classes.vue', () => {
   it.each([
+    [0, ['classes__title']],
     [1, ['classes__title', 'classes__title--primary']],
     [2, ['classes__title', 'classes__title--warning']],
   ])('has correct class', (inputNumber, expectedClasses) => {
